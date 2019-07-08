9e98dec3e92c7693077810190253a829e1f0f513
Arthur-Arutyunyan <Arthur.Arutyunyan@ibm.com>
2019-07-08 13:12:32 -0500

    Setup json server

--- schema/schema.js
+++ schema/schema.js
@@ -1,8 +1,10 @@
 const graphql = require('graphql');
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
+    GraphQLSchema,
 } = graphql;
 
 const UserType = new GraphQLObjectType({
@@ -12,4 +14,22 @@ const UserType = new GraphQLObjectType({
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt }
     }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                  .then(resp => resp.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
